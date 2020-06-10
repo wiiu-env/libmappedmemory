e27ca247ae559becbb3f90bcfc460cb2dda1ccfe
Maschell <Maschell@gmx.de>
2020-06-10 15:33:15 +0200

    Replace alloc/free function export by data exports as function pointers.

--- include/memory/mappedmemory.h
+++ include/memory/mappedmemory.h
@@ -7,13 +7,13 @@ extern "C"
 
 #include <cstdint>
 
-extern void MemoryMappingFree(void* ptr);
-
-extern void* MemoryMappingAlloc(uint32_t size, uint32_t align);
-
 extern uint32_t MemoryMappingEffectiveToPhysical(uint32_t address);
 
 extern uint32_t MemoryMappingPhysicalToEffective(uint32_t address);
+
+extern void *(*MEMAllocFromMappedMemory)(uint32_t size);
+extern void *(*MEMAllocFromMappedMemoryEx)(uint32_t size, int32_t alignment);
+extern void (*MEMFreeToMappedMemory)(void *ptr);
 
 #ifdef __cplusplus
 } // extern "C"
