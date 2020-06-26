ce6980db5675c1725c73449a38b12e3a26f7fc69
Maschell <Maschell@gmx.de>
2020-06-26 20:33:52 +0200

    Add "MEMAllocFromMappedMemoryForGX2Ex"

--- include/memory/mappedmemory.h
+++ include/memory/mappedmemory.h
@@ -13,6 +13,7 @@ extern uint32_t MemoryMappingPhysicalToEffective(uint32_t address);
 
 extern void *(*MEMAllocFromMappedMemory)(uint32_t size);
 extern void *(*MEMAllocFromMappedMemoryEx)(uint32_t size, int32_t alignment);
+extern void *(*MEMAllocFromMappedMemoryForGX2Ex)(uint32_t size, int32_t alignment);
 extern void (*MEMFreeToMappedMemory)(void *ptr);
 
 #ifdef __cplusplus
